7f8cdbbb3e6841b32aa09191e9f47f1d02fd272a
Martin <martin.lu.cam@gmail.com>
2026-02-01 15:14:14 -0800

    fix: many style fixes, migrate radix tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import { SessionProviderWrapper } from "@/components/SessionProviderWrapper"
 import { Providers } from "./providers"
 import { StyleDebugger } from "@/components/StyleDebugger"
+import { Theme } from "@radix-ui/themes"
+import "@radix-ui/themes/styles.css"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -13,10 +15,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <StyleDebugger />
-        <SessionProviderWrapper>
-          <Providers>{children}</Providers>
-        </SessionProviderWrapper>
+        <Theme accentColor="iris" grayColor="slate" radius="medium">
+          <StyleDebugger />
+          <SessionProviderWrapper>
+            <Providers>{children}</Providers>
+          </SessionProviderWrapper>
+        </Theme>
       </body>
     </html>
   )
