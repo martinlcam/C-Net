9717caf068d240a41832986c7b4368c16d7610ff
martindesktop <martinlucam@gmail.com>
2026-07-07 01:40:52 -0700

    proxpush

--- playground.ts
+++ playground.ts
@@ -1,5 +1,17 @@
+async function calcProfitLoss(startingMoney: number, leavingMoney: number) {
+  const profitLoss = leavingMoney - startingMoney
+  return profitLoss
+}
+
+
 async function main() {
-  console.log("Hello, world!")
+  const result = await calcProfitLoss(100, 150)
+  console.log(result) // 50 profit
+
+  const loss = await calcProfitLoss(100, 75)
+  console.log(loss) // -25 loss
 }
 
 main()
+
+"Would you hesitate to throw a bundle of logs onto a fire... because you pitied the tree they came from?"
